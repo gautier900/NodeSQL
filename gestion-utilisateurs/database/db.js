9e58c38e4d5822faf9b801f3e0d92e9522edc6f2
Gautier Beley <gautier.beley06@gmail.com>
2025-11-10 13:40:18 +0100

    TASK 130

--- gestion-utilisateurs/database/db.js
+++ gestion-utilisateurs/database/db.js
@@ -3,11 +3,11 @@ const { Pool } = require("pg");
 require("dotenv").config();
 
 const pool = new Pool({
-  user: DB_USER,
-  host: DB_HOST,
-  database: DB_NAME,
-  password: DB_PASSWORD,
-  port: DB_PORT,
+  user: process.env.DB_USER,
+  host: process.env.DB_HOST,
+  database: process.env.DB_NAME,
+  password: process.env.DB_PASSWORD,
+  port: process.env.DB_PORT,
 });
 
 pool.on("connect", () => {
